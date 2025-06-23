5f7c4b5e81e9bf2d51680d76efbd15666ce5cc97
Emmanuel Oluwatayese <adetutuemmanueloluwatayese@gmail.com>
2025-06-23 10:53:26 +0100

    feat: created tollbar component

---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,38 @@
+// src/components/Toolbar.jsx
+import { useEditor } from '../context/EditorContext';
+import { BLOCK_TYPES } from '../lib/blockTypes';
+
+export const Toolbar = () => {
+  const { addBlock, blocks } = useEditor();
+
+  return (
+    <div className="bg-gray-100 p-4 sticky top-0 z-10">
+      <div className="flex space-x-2">
+        <button
+          onClick={() => addBlock(BLOCK_TYPES.HEADING, blocks.length - 1)}
+          className="px-3 py-1 bg-white border rounded hover:bg-gray-50"
+        >
+          Add Heading
+        </button>
+        <button
+          onClick={() => addBlock(BLOCK_TYPES.PARAGRAPH, blocks.length - 1)}
+          className="px-3 py-1 bg-white border rounded hover:bg-gray-50"
+        >
+          Add Paragraph
+        </button>
+        <button
+          onClick={() => addBlock(BLOCK_TYPES.IMAGE, blocks.length - 1)}
+          className="px-3 py-1 bg-white border rounded hover:bg-gray-50"
+        >
+          Add Image
+        </button>
+        <button
+          onClick={() => addBlock(BLOCK_TYPES.VIDEO, blocks.length - 1)}
+          className="px-3 py-1 bg-white border rounded hover:bg-gray-50"
+        >
+          Add Video
+        </button>
+      </div>
+    </div>
+  );
+};
